6291ae4e566aa99c44596bc77537d0fd66ab9d21
daiquyfunny <sarwan.quy11@gmail.com>
2024-04-02 01:25:18 +0700

    Added circle calculation which include its periemter and area

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+const double pi = 3.1415926;
 
 double calculate(int operation, double n1, double n2) {
   double result;
@@ -24,40 +27,74 @@ double calculate(int operation, double n1, double n2) {
   return result;
 }
 
-int main(void) {
+int userinputI(char *prompt) {
   int a;
-  double n1, n2;
-  char input[1024], na[1024], nb[1024];
+  char x[1024];
+
+  printf("%s: ", prompt);
+  do {
+    if (!fgets(x, sizeof(x), stdin)) {
+      return 1; 
+    }
+  } while (x == 0);
   
-  printf("1. addtion 2. subtraction 3. multiplication 4. division\n");
+  a = atoi(x);
+  return a;
+}
+
+double userinputD(char *prompt) {
+  double a;
+  char x[1024];
+
+  printf("%s: ", prompt);
   do {
-    printf("operation: ");
-    if (!fgets(input, 1024, stdin)) {
-      return 1;
+    if (!fgets(x, sizeof(x), stdin)) {
+      return 1; 
     }
+  } while (x == 0);
+  
+  a = strtod(x, NULL);
+  return a;
+}
 
-    a = atoi(input);
-  } while (a == 0);
+struct circle {
+  double perimeter;
+  double area;
+};
 
-  do {
-    printf("na: ");
-    if (!fgets(na, 1024, stdin)) {
+struct circle fc(char *prompt) {
+  double radius = userinputD(prompt);
+  double perimeter = 2*pi*radius;
+  double area = pi*pow(radius, 2);
+  struct circle nc = {perimeter, area};
+  return nc;
+}
+
+int main(void) {
+  int a, input;
+  double n1, n2;
+  
+  printf("1. arithmetic 2. circle\n");
+  input = userinputI("input: ");
+  switch(input) {
+    case 1:
+      printf("1. addtion 2. subtraction 3. multiplication 4. division\n");
+      a = userinputI("operation: ");
+      n1 = userinputD("n1: ");
+      n2 = userinputD("n2: ");
+
+      printf("result: %lf", calculate(a, n1, n2));
+      break;
+    case 2:
+      {
+        struct circle nc = fc("radius: ");
+        printf("perimeter: %lf\narea: %lf", nc.perimeter, nc.area);
+      }
+      break;
+    default:
       return 1;
-    }
-
-    n1 = strtod(na, NULL);
-  } while (n1 == 0);
-
-  do {
-    printf("nb: ");
-    if (!fgets(nb, 1024, stdin)) {
-      return 1;
-    }
-
-    n2 = strtod(nb, NULL);
-  } while (n2 == 0);
-
-  printf("%lf", calculate(a, n1, n2));
+      break;
+  }
   
   return 0;
 }
